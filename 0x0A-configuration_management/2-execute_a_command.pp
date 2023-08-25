1667650e8f2317d547088f9f7871b8a75048385e
OsiroA <124213013+OsiroA@users.noreply.github.com>
2023-08-25 16:48:37 +0100

    Update 2-execute_a_command.pp
    Changed the source line

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # This puppet script creates a manifest that kills a process named 'killmenow'
 exec { 'kill':
   command => 'pkill killmenow',
-  path    => 'usr/bin',
+  path    => ['/usr/bin', '/usr/sbin'],
 }
