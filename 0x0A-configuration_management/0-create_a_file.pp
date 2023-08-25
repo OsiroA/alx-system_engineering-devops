b13ccee2ecc435bcccf16a955d86e714e1bf59b2
OsiroA <osiroasunde@gmail.com>
2023-08-25 00:59:54 -0700

    uploading the first puppet file so I can try with a different terminal

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# this script creates a file in /tmp
+file { '/tmp/school.txt':
+  ensure => file,
+  owner  => 'www-data',
+  group  => 'www-data',
+  mode   => '0744',
+  content => 'I love Puppet',
+}
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,1 @@
+This directory contains tasks on Configuartion management using Puppet, taught by Holberton and ALX
