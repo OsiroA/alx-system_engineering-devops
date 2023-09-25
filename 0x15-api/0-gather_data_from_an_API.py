5d999a616a762de7928a430fc75fbc02dc2d6da5
OsiroA <124213013+OsiroA@users.noreply.github.com>
2023-09-25 21:54:41 +0100

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,45 +3,31 @@
 This gathers data from an API, and then uses it to creqate a todo
 list progress function
 '''
-
-
 import requests
 import sys
 
 
 def todoList(employee_id):
     '''This function does the requested task in the earlier comment'''
-    # Define the API endpoint URL
-    api_url = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
+    api_url = 'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
 
     try:
-        # Send a GET request to the API
+        
         response = requests.get(api_url)
-
-        # Check if the request was successful
         if response.status_code == 200:
-            # Parse the JSON response
             todos = response.json()
-
-            # Filter completed tasks
             completed_tasks = [todo for todo in todos if todo['completed']]
-
-            # Get employee name
             employee_name = todos[0]['user']['name']
-
-            # Calculate progress
             total_tasks = len(todos)
             completed_task_count = len(completed_tasks)
             tasks_left = total_tasks - completed_task_count
-
-            # Display progress information
-            print(f'Employee {employee_name} is done with tasks ({completed_task_count}/{total_tasks}):')
-            print(f'{employee_name} has completed {completed_task_count} tasks.')
-            print(f'{employee_name} has {tasks_left} tasks left to do.')
+            print('Employee {} is done with tasks ({}/{}):'.format(employee_name, completed_task_count, total_tasks))
+            print('{} has completed {} tasks.'.format(employee_name, completed_task_count))
+            print('{} has {} tasks left to do.'.format(employee_name, tasks_left))
         else:
-            print(f"Failed to retrieve data. Status code: {response.status_code}")
+            print("Failed to retrieve data. Status code: {}".format(response.status_code))
     except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
+        print("Error: {}".format(e))
 
 if __name__ == '__main__':
     employee_id = int(input("Enter Employee ID: "))
